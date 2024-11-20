dc3d49eb52e1b00507e1cfd8a8a4c9a15e557a6c
NFFJ25 <16523106@std.stei.itb.ac.id>
2024-11-20 17:38:22 +0700

    add: new templates for login and register, also split index.html into login.html and register.html

--- login_register/main.py
+++ login_register/main.py
@@ -1,30 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('login.html')
 
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
-    # Add logic to validate user login here
-    return f"Welcome back, {username}!"
-
-@app.route('/register', methods=['POST'])
+@app.route('/register')
 def register():
-    username = request.form['username']
-    email = request.form['email']
-    password = request.form['password']
-    confirm_password = request.form['confirm_password']
-    
-    if password != confirm_password:
-        return "Passwords do not match!", 400
-    
-    # Add logic to register the user here
-    return f"Account created for {username} with email {email}!"
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
